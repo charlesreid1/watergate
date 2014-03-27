9b9f1d919b9a97c5f8b23d985cfb8635e8de811f
Charles Reid <charlesreid1@gmail.com>
2014-03-26 23:14:30 -0700

    updating nltk script. making progress toward splitting headers and dialogue up.

--- 3extractjson/nltk_toy.py
+++ 3extractjson/nltk_toy.py
@@ -13,12 +13,12 @@ Tokenizes it with natural language toolkit NLTK.
 This is to perfect the technique.
 """
 
-datdir = '../1primarytexts/'
+datdir = '../1primarytexts/txt/'
 
 # get list of txt files
 filenames = []
-for f in os.listdir(datdir):
-    if f.endswith(".txt"):
+for nf,f in enumerate( os.listdir(datdir) ):
+    if f.endswith(".txt") and nf==0:
         filenames.append( basename( splitext(f)[0] ) )
 
 for myfile in filenames:
@@ -35,14 +35,22 @@ for myfile in filenames:
     #
     # On 118 text files.
 
-    # Plain ol nltk word tokenizer
-    # http://stackoverflow.com/questions/15057945/how-do-i-tokenize-a-string-sentence-in-nltk
-    tokens1 = nltk.word_tokenize(data)
-    #print tokens1
-    
     # re for all-caps words (i.e., the names)
     # - RegexpTokenizer: https://nltk.googlecode.com/svn/trunk/doc/howto/tokenize.html
     # - the regex: http://stackoverflow.com/questions/4598315/regex-to-match-only-uppercase-words-with-some-exceptions
     tokens2 = RegexpTokenizer('[A-Z]{3,}').tokenize(data)
-    #print unique(tokens2)
+    print unique(tokens2)
+    for name in tokens2:
+        data = data.replace(name,' '+name+'\n')
+
+    # Plain ol nltk word tokenizer
+    # http://stackoverflow.com/questions/15057945/how-do-i-tokenize-a-string-sentence-in-nltk
+    tokens1 = nltk.word_tokenize(data)
+    print sort(tokens1)
+
+    ### # Header is everything up until first occurrence
+    ### # of a name.
+    ### first_occurs = [tokens1.index(name) for name in tokens2]
+    ### first_occur = min(first_occurrences)
+    ### header = tokens1[:first_occur]
 
